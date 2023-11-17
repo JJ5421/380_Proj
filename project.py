46e1268a487bdb322fa3bc85b4eddd0e37f98f9d
JJKuznetsov <jeremykuz03@gmail.com>
2023-11-17 13:52:13 -0500

    Project

---
+++ project.py
@@ -0,0 +1,61 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import keyboard
+
+def main():
+	
+    try:
+        while True:
+            
+            print("yomamaphone")
+
+            
+            if keyboard.is_pressed('esc'):
+                print("Exiting loop.")
+                break
+
+    except KeyboardInterrupt:
+        pass
+
+def readim():
+
+    pass
+
+def cascade(cascade, im):
+
+    pass
+
+def center(tl_point, width):
+
+    pass
+
+def calc_pos(center):
+
+    pass
+
+def calc_adjust(x, y, size):
+
+    pass
+
+def pos_adjust(dx, dy, magnitude):
+
+    pass
+
+def sweep():
+
+    pass
+
+def greyscale():
+
+    pass
+
+def blur():
+
+    pass
+
+def canny():
+
+    pass
+
+main()
